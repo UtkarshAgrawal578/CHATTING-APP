a68c3cb43710f4168a9b7804d3bbc7bf8a26454f
UtkarshAgrawal578 <utkarshzhcet1234@gmail.com>
2026-04-21 01:02:51 +0530

    successfully deployed

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-backend-qocw.onrender.com");
 
 function Chat({ user, setUser }) {
   const [text, setText] = useState("");
